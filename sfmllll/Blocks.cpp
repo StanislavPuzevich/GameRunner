352e8deca83a543be0972fef3883ec247ab7e4b2
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-01-31 14:54:09 +0300

    Update Blocks.cpp

--- sfmllll/Blocks.cpp
+++ sfmllll/Blocks.cpp
@@ -1,187 +1,139 @@
-#include <SFML/Graphics.hpp>
 #include <cstdlib>
 #include "Blocks.h"
 
 using namespace sf;
 
-void Blocks::loadFile()                                          // подключение изображений       
+void Blocks::loadFile()                                          // РїРѕРґРєР»СЋС‡РµРЅРёРµ РёР·РѕР±СЂР°Р¶РµРЅРёР№       
 {
 	b.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/Block.png");
 	c.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/Cactus.png");
 	sm.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/SnowMan.png");
 }
 
-void Blocks::randBlockObj()                                     // случайная генерация БЛОКОВ на 1 уровне сразу после начала игры
+void Blocks::randBlockObj()                                     // СЃР»СѓС‡Р°Р№РЅР°СЏ РіРµРЅРµСЂР°С†РёСЏ Р‘Р›РћРљРћР’ РЅР° 1 СѓСЂРѕРІРЅРµ СЃСЂР°Р·Сѓ РїРѕСЃР»Рµ РЅР°С‡Р°Р»Р° РёРіСЂС‹
 {
-	srand(static_cast<unsigned>(time(nullptr))); 
-	for (int i = 0; i < 5; i++)                                 // цикл на 5 элементов, по логике игры на экране одновременно долдно быть <=5 блоков
+	srand(static_cast<unsigned>(time(nullptr)));
+	for (int i = 0; i < 5; i++)                                 // С†РёРєР» РЅР° 5 СЌР»РµРјРµРЅС‚РѕРІ, РїРѕ Р»РѕРіРёРєРµ РёРіСЂС‹ РЅР° СЌРєСЂР°РЅРµ РѕРґРЅРѕРІСЂРµРјРµРЅРЅРѕ РґРѕР»РґРЅРѕ Р±С‹С‚СЊ <=5 Р±Р»РѕРєРѕРІ
 	{
-		block[i].setTexture(b);                                 // накладывает текстуру на спрайт
-		block[i].setScale(0.8f, 0.8f);                          // уменьшает размер спрайта 
-		bx[i] = rand() % 3;                                     // изображение имеет 6 видов блоков (3х2) bx - движение по х
-		by[i] = rand() % 2;                                     // движение по у
-		bty[i] = rand() % 3;                                    // задаёт случайное расположение блоков на "беговых уровнях" (1,2,3)
-		block[i].setTextureRect(IntRect(158 * bx[i], 158 * by[i], 158, 155));  // случайный выбор блока на изображении
-		block[i].setPosition(1920 + 220 * static_cast<float>(i), 235 + 335 * static_cast<float>(bty[i])); // случайное расположение блока в игре
+		block[i].setTexture(b);                                 // РЅР°РєР»Р°РґС‹РІР°РµС‚ С‚РµРєСЃС‚СѓСЂСѓ РЅР° СЃРїСЂР°Р№С‚
+		block[i].setScale(0.8f, 0.8f);                          // СѓРјРµРЅСЊС€Р°РµС‚ СЂР°Р·РјРµСЂ СЃРїСЂР°Р№С‚Р° 
+		bx[i] = rand() % 3;                                     // РёР·РѕР±СЂР°Р¶РµРЅРёРµ РёРјРµРµС‚ 6 РІРёРґРѕРІ Р±Р»РѕРєРѕРІ (3С…2) bx - РґРІРёР¶РµРЅРёРµ РїРѕ С…
+		by[i] = rand() % 2;                                     // РґРІРёР¶РµРЅРёРµ РїРѕ Сѓ
+		bty[i] = rand() % 3;                                    // Р·Р°РґР°С‘С‚ СЃР»СѓС‡Р°Р№РЅРѕРµ СЂР°СЃРїРѕР»РѕР¶РµРЅРёРµ Р±Р»РѕРєРѕРІ РЅР° "Р±РµРіРѕРІС‹С… СѓСЂРѕРІРЅСЏС…" (1,2,3)
+		block[i].setTextureRect(IntRect(158 * bx[i], 158 * by[i], 158, 155));  // СЃР»СѓС‡Р°Р№РЅС‹Р№ РІС‹Р±РѕСЂ Р±Р»РѕРєР° РЅР° РёР·РѕР±СЂР°Р¶РµРЅРёРё
+		block[i].setPosition(1920 + 250 * static_cast<float>(i), 235 + 335 * static_cast<float>(bty[i])); // СЃР»СѓС‡Р°Р№РЅРѕРµ СЂР°СЃРїРѕР»РѕР¶РµРЅРёРµ Р±Р»РѕРєР° РІ РёРіСЂРµ
 	}
 }
 
-void Blocks::randCactusObj()                                    // случайная генерация КАКТУСОВ на 2 уровне сразу после начала игры
+void Blocks::randCactusObj()                                    // СЃР»СѓС‡Р°Р№РЅР°СЏ РіРµРЅРµСЂР°С†РёСЏ РљРђРљРўРЈРЎРћР’ РЅР° 2 СѓСЂРѕРІРЅРµ СЃСЂР°Р·Сѓ РїРѕСЃР»Рµ РЅР°С‡Р°Р»Р° РёРіСЂС‹
 {
 	srand(static_cast<unsigned>(time(nullptr)));
 	for (int i = 0; i < 5; i++)
 	{
-		cactus[i].setTexture(c);                                // накладывает текстуру на спрайт
-		cactus[i].setScale(0.25f, 0.25f);                       // уменьшает размер спрайта 
-		bx[i] = rand() % 3;                                     // изображение имеет 3 видов кактусов
+		cactus[i].setTexture(c);                                // РЅР°РєР»Р°РґС‹РІР°РµС‚ С‚РµРєСЃС‚СѓСЂСѓ РЅР° СЃРїСЂР°Р№С‚
+		cactus[i].setScale(0.25f, 0.25f);                       // СѓРјРµРЅСЊС€Р°РµС‚ СЂР°Р·РјРµСЂ СЃРїСЂР°Р№С‚Р° 
+		bx[i] = rand() % 3;                                     // РёР·РѕР±СЂР°Р¶РµРЅРёРµ РёРјРµРµС‚ 3 РІРёРґРѕРІ РєР°РєС‚СѓСЃРѕРІ
 		bty[i] = rand() % 3;
-		cactus[i].setTextureRect(IntRect(550 * bx[i], 10, 545, 700));  
-		cactus[i].setPosition(1920 + 310 * static_cast<float>(i), 150 + 330 * static_cast<float>(bty[i])); 
+		cactus[i].setTextureRect(IntRect(550 * bx[i], 10, 545, 700));
+		cactus[i].setPosition(1920 + 310 * static_cast<float>(i), 150 + 330 * static_cast<float>(bty[i]));
 	}
 }
 
-void Blocks::randSnowManObj()                                    // случайная генерация СНЕГОВИКОВ на 3 уровне сразу после начала игры
+void Blocks::randSnowManObj()                                    // СЃР»СѓС‡Р°Р№РЅР°СЏ РіРµРЅРµСЂР°С†РёСЏ РЎРќР•Р“РћР’РРљРћР’ РЅР° 3 СѓСЂРѕРІРЅРµ СЃСЂР°Р·Сѓ РїРѕСЃР»Рµ РЅР°С‡Р°Р»Р° РёРіСЂС‹
 {
 	srand(static_cast<unsigned>(time(nullptr)));
 	for (int i = 0; i < 5; i++)
 	{
-		SnowMan[i].setTexture(sm);                               // накладывает текстуру на спрайт
-		SnowMan[i].setScale(0.45f, 0.45f);                       // уменьшает размер спрайта 
-		bx[i] = rand() % 3;                                      // изображение имеет 3 видов снеговиков
+		SnowMan[i].setTexture(sm);                               // РЅР°РєР»Р°РґС‹РІР°РµС‚ С‚РµРєСЃС‚СѓСЂСѓ РЅР° СЃРїСЂР°Р№С‚
+		SnowMan[i].setScale(0.45f, 0.45f);                       // СѓРјРµРЅСЊС€Р°РµС‚ СЂР°Р·РјРµСЂ СЃРїСЂР°Р№С‚Р° 
+		bx[i] = rand() % 3;                                      // РёР·РѕР±СЂР°Р¶РµРЅРёРµ РёРјРµРµС‚ 3 РІРёРґРѕРІ СЃРЅРµРіРѕРІРёРєРѕРІ
 		bty[i] = rand() % 3;
 		SnowMan[i].setTextureRect(IntRect(440 * bx[i], 0, 430, 500));
 		SnowMan[i].setPosition(1920 + 600 * static_cast<float>(i), 140 + 320 * static_cast<float>(bty[i]));
 	}
 }
 
-int Blocks::randMoveBlockObj(Sprite man)                         // случайная генерация БЛОКОВ на 1 уровне с движением
+int Blocks::randMoveBlockObj(Sprite man)                         // СЃР»СѓС‡Р°Р№РЅР°СЏ РіРµРЅРµСЂР°С†РёСЏ Р‘Р›РћРљРћР’ РЅР° 1 СѓСЂРѕРІРЅРµ СЃ РґРІРёР¶РµРЅРёРµРј
 {
 	srand(static_cast<unsigned>(time(nullptr)));
 	Vector2f bk[5];
 	for (int i = 0; i < 5; i++)
 	{
-		block[i].move(-1.0, 0);                                  // движение по Х                 
-		block[i].setScale(0.8f, 0.8f);                           // масштаб спрайта
-		bk[i] = block[i].getPosition();   
-		if (bk[i].x < -100)                                      // как только координата по Х = -100 объект пропадает с экрана и появляется новый
+		block[i].move(-1.f, 0);                                  // РґРІРёР¶РµРЅРёРµ РїРѕ РҐ                 
+		block[i].setScale(0.8f, 0.8f);                           // РјР°СЃС€С‚Р°Р± СЃРїСЂР°Р№С‚Р°
+		bk[i] = block[i].getPosition();
+		if (bk[i].x < -100)                                      // РєР°Рє С‚РѕР»СЊРєРѕ РєРѕРѕСЂРґРёРЅР°С‚Р° РїРѕ РҐ = -100 РѕР±СЉРµРєС‚ РїСЂРѕРїР°РґР°РµС‚ СЃ СЌРєСЂР°РЅР° Рё РїРѕСЏРІР»СЏРµС‚СЃСЏ РЅРѕРІС‹Р№
 		{
-			bx[i] = rand() % 3;                                  // изображение имеет 6 видов блоков (3х2)
+			bx[i] = rand() % 3;                                  // РёР·РѕР±СЂР°Р¶РµРЅРёРµ РёРјРµРµС‚ 6 РІРёРґРѕРІ Р±Р»РѕРєРѕРІ (3С…2)
 			by[i] = rand() % 2;
 			bty[i] = rand() % 3;
 			block[i].setTextureRect(IntRect(158 * bx[i], 158 * by[i], 158, 155));
 			block[i].setPosition(1920 + 160 * static_cast<float>(i), 235 + 335 * static_cast<float>(bty[i]));
 		}
-		if (man.getGlobalBounds().intersects(block[i].getGlobalBounds()))    // проверка на столкновение героя с препятствием
-			return 2;                                            // если столкнулись возвращаем значение 2 и игра завершается
+		if (man.getGlobalBounds().intersects(block[i].getGlobalBounds()))    // РїСЂРѕРІРµСЂРєР° РЅР° СЃС‚РѕР»РєРЅРѕРІРµРЅРёРµ РіРµСЂРѕСЏ СЃ РїСЂРµРїСЏС‚СЃС‚РІРёРµРј
+			return 2;                                            // РµСЃР»Рё СЃС‚РѕР»РєРЅСѓР»РёСЃСЊ РІРѕР·РІСЂР°С‰Р°РµРј Р·РЅР°С‡РµРЅРёРµ 2 Рё РёРіСЂР° Р·Р°РІРµСЂС€Р°РµС‚СЃСЏ
 	}
-	return 0;                                                    // если столкновения не было возвращаем 0
+	return 0;                                                    // РµСЃР»Рё СЃС‚РѕР»РєРЅРѕРІРµРЅРёСЏ РЅРµ Р±С‹Р»Рѕ РІРѕР·РІСЂР°С‰Р°РµРј 0
 }
 
-int Blocks::randMoveCactusObj(Sprite man)                        // случайная генерация КАКТУСОВ на 2 уровне с движением
+int Blocks::randMoveCactusObj(Sprite man)                        // СЃР»СѓС‡Р°Р№РЅР°СЏ РіРµРЅРµСЂР°С†РёСЏ РљРђРљРўРЈРЎРћР’ РЅР° 2 СѓСЂРѕРІРЅРµ СЃ РґРІРёР¶РµРЅРёРµРј
 {
 	srand(static_cast<unsigned>(time(nullptr)));
 	Vector2f bk[5];
 	for (int i = 0; i < 5; i++)
 	{
-		cactus[i].setScale(0.25f, 0.25f);                        // масштаб спрайта
-		cactus[i].move(-1.1f, 0);                                // скорость движения по оси Х
+		cactus[i].setScale(0.25f, 0.25f);                        // РјР°СЃС€С‚Р°Р± СЃРїСЂР°Р№С‚Р°
+		cactus[i].move(-1.1f, 0);                                // СЃРєРѕСЂРѕСЃС‚СЊ РґРІРёР¶РµРЅРёСЏ РїРѕ РѕСЃРё РҐ
 		bk[i] = cactus[i].getPosition();
-		if (bk[i].x < -100)                                      // как только координата по Х = -100 объект пропадает с экрана и появляется новый
+		if (bk[i].x < -100)                                      // РєР°Рє С‚РѕР»СЊРєРѕ РєРѕРѕСЂРґРёРЅР°С‚Р° РїРѕ РҐ = -100 РѕР±СЉРµРєС‚ РїСЂРѕРїР°РґР°РµС‚ СЃ СЌРєСЂР°РЅР° Рё РїРѕСЏРІР»СЏРµС‚СЃСЏ РЅРѕРІС‹Р№
 		{
-			bx[i] = rand() % 3;                                  // изображение имеет 3 видов кактусов
+			bx[i] = rand() % 3;                                  // РёР·РѕР±СЂР°Р¶РµРЅРёРµ РёРјРµРµС‚ 3 РІРёРґРѕРІ РєР°РєС‚СѓСЃРѕРІ
 			bty[i] = rand() % 3;
 			cactus[i].setTextureRect(IntRect(550 * bx[i], 10, 550, 700));
 			cactus[i].setPosition(1920 + 280 * static_cast<float>(i), 150 + 330 * static_cast<float>(bty[i]));
 		}
-		if (man.getGlobalBounds().intersects(cactus[i].getGlobalBounds()))    // проверка на столкновение героя с препятствием
+		if (man.getGlobalBounds().intersects(cactus[i].getGlobalBounds()))    // РїСЂРѕРІРµСЂРєР° РЅР° СЃС‚РѕР»РєРЅРѕРІРµРЅРёРµ РіРµСЂРѕСЏ СЃ РїСЂРµРїСЏС‚СЃС‚РІРёРµРј
 			return 2;
 	}
 	return 0;
 }
 
-int Blocks::randMoveSnowManObj(Sprite man)                        // случайная генерация СНЕГОВИКОВ на 1 уровне с движением
+int Blocks::randMoveSnowManObj(Sprite man)                        // СЃР»СѓС‡Р°Р№РЅР°СЏ РіРµРЅРµСЂР°С†РёСЏ РЎРќР•Р“РћР’РРљРћР’ РЅР° 1 СѓСЂРѕРІРЅРµ СЃ РґРІРёР¶РµРЅРёРµРј
 {
 	srand(static_cast<unsigned>(time(nullptr)));
 	Vector2f bk[5];
 	for (int i = 0; i < 5; i++)
 	{
-		SnowMan[i].setScale(0.5f, 0.5f);                          // масштаб спрайта
-		SnowMan[i].move(-1.2f, 0);                                // скорость движения по оси Х
+		SnowMan[i].setScale(0.5f, 0.5f);                          // РјР°СЃС€С‚Р°Р± СЃРїСЂР°Р№С‚Р°
+		SnowMan[i].move(-1.2f, 0);                                // СЃРєРѕСЂРѕСЃС‚СЊ РґРІРёР¶РµРЅРёСЏ РїРѕ РѕСЃРё РҐ
 		bk[i] = SnowMan[i].getPosition();
-		if (bk[i].x < -440)                                       // как только координата по Х = -100 объект пропадает с экрана и появляется новый
+		if (bk[i].x < -440)                                       // РєР°Рє С‚РѕР»СЊРєРѕ РєРѕРѕСЂРґРёРЅР°С‚Р° РїРѕ РҐ = -100 РѕР±СЉРµРєС‚ РїСЂРѕРїР°РґР°РµС‚ СЃ СЌРєСЂР°РЅР° Рё РїРѕСЏРІР»СЏРµС‚СЃСЏ РЅРѕРІС‹Р№
 		{
 			bx[i] = rand() % 3;
 			bty[i] = rand() % 3;
 			SnowMan[i].setTextureRect(IntRect(440 * bx[i], 0, 430, 500));
 			SnowMan[i].setPosition(1920 + 420 * static_cast<float>(i), 140 + 320 * static_cast<float>(bty[i]));
-		} 
-		if (man.getGlobalBounds().intersects(SnowMan[i].getGlobalBounds()))    // проверка на столкновение героя с препятствием
+		}
+		if (man.getGlobalBounds().intersects(SnowMan[i].getGlobalBounds()))    // РїСЂРѕРІРµСЂРєР° РЅР° СЃС‚РѕР»РєРЅРѕРІРµРЅРёРµ РіРµСЂРѕСЏ СЃ РїСЂРµРїСЏС‚СЃС‚РІРёРµРј
 			return 2;
 	}
 	return 0;
 }
 
-void Blocks::draw(RenderWindow& window)            // ф-я отображения БЛОКОВ на экране
+void Blocks::draw(RenderWindow& window)            // С„-СЏ РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ Р‘Р›РћРљРћР’ РЅР° СЌРєСЂР°РЅРµ
 {
 	for (const auto& i : block)
 		window.draw(i);
 }
 
-void Blocks::drawCactus(RenderWindow& window)      // ф-я отображения КАКТУСОВ на экране
+void Blocks::drawCactus(RenderWindow& window)      // С„-СЏ РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ РљРђРљРўРЈРЎРћР’ РЅР° СЌРєСЂР°РЅРµ
 {
 	for (const auto& i : cactus)
 		window.draw(i);
 }
 
-void Blocks::drawSnowMan(RenderWindow& window)    // ф-я отображения СНЕГОВИКОВ на экране
+void Blocks::drawSnowMan(RenderWindow& window)    // С„-СЏ РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ РЎРќР•Р“РћР’РРљРћР’ РЅР° СЌРєСЂР°РЅРµ
 {
 	for (const auto& i : SnowMan)
 		window.draw(i);
 }
-
-
-//Проект готов! однако хочу изменить геометрию объектов чуть-чуть
-
-//for (int i = 0; i < 5; i++)
-//{
-//	SnowMa[i].setTexture(&sm);
-//	SnowMa[i].setScale(0.4f, 0.4f);
-//	SnowMa[i].setPointCount(6);
-//	SnowMa[i].setPoint(0, Vector2f(90, 0));
-//	SnowMa[i].setPoint(1, Vector2f(340, 0));
-//	SnowMa[i].setPoint(2, Vector2f(0, 200));
-//	SnowMa[i].setPoint(3, Vector2f(430, 200));
-//	SnowMa[i].setPoint(4, Vector2f(0, 500));
-//	SnowMa[i].setPoint(5, Vector2f(430, 500));
-//	bx[i] = rand() % 3;
-//	bty[i] = rand() % 3;
-//	SnowMa[i].setTextureRect(IntRect(440 * bx[i], 0, 430, 500));
-//	SnowMa[i].setPosition(1920 + 600 * static_cast<float>(i), 140 + 300 * static_cast<float>(bty[i]));
-//}
-
-//for (int i = 0; i < 5; i++)
-	//{
-	//	SnowMa[i].setTexture(&sm);
-	//	SnowMa[i].setScale(0.4f, 0.4f);
-	//	SnowMa[i].move(-0.9f, 0);
-	//	bk[i] = SnowMa[i].getPosition();
-
-	//	if (bk[i].x < -440)
-	//	{
-	//		SnowMa[i].setPointCount(6);
-	//		SnowMa[i].setPoint(0, Vector2f(180, 0));
-	//		SnowMa[i].setPoint(1, Vector2f(200, 0));
-	//		SnowMa[i].setPoint(2, Vector2f(430, 200));
-	//		SnowMa[i].setPoint(3, Vector2f(430, 500));
-	//		SnowMa[i].setPoint(4, Vector2f(0, 500));
-	//		SnowMa[i].setPoint(5, Vector2f(0, 200));
-	//	
-	//		bx[i] = rand() % 3;
-	//		bty[i] = rand() % 3;
-	//		SnowMa[i].setTextureRect(IntRect(440 * bx[i], 0, 430, 500));
-	//		SnowMa[i].setPosition(1920 + 600 * static_cast<float>(i), 140 + 300 * static_cast<float>(bty[i]));
-	//	}
-	//	if (man.getGlobalBounds().intersects(SnowMa[i].getGlobalBounds()))
-	//			return 2;
-	//}
-	//return 0;
