6beda273d30b7311904efe3f685a977888a22102
Stanislav <72744333+StanislavPuzevich@users.noreply.github.com>
2022-01-31 15:07:36 +0300

    Update Location1.cpp

--- sfmllll/Location1.cpp
+++ sfmllll/Location1.cpp
@@ -1,6 +1,5 @@
 #include "Blocks.h"
 #include "Earth.h"
-#include "Score.h"
 #include "Boost.h"
 
 using namespace  sf;
@@ -11,105 +10,105 @@ void UpDownMovement(int& game, Event& event, Sound& jump, Music& pl, bool& down,
 
 void Location1(int& GameLocation, RenderWindow& window)
 {
-    Music pl;                                                     // èãðîâàÿ ìåëîäèÿ
+    Music pl;                                                     // игровая мелодия
     pl.openFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/Game.wav");
-    pl.setVolume(60);                                             // ãðîìêîñòü 
-    pl.setLoop(true);                                             // áåñêîíå÷íûé ïîâòîð
+    pl.setVolume(60.f);                                           // громкость 
+    pl.setLoop(true);                                             // бесконечный повтор
 
-    SoundBuffer jm;                                               // çâóê ïðûæêà
+    SoundBuffer jm;                                               // звук прыжка
     jm.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/Jump.wav");
     Sound jump(jm);
-    jump.setVolume(10);
+    jump.setVolume(10.f);
 
-    SoundBuffer dr;                                               // çâóê êàïëè
+    SoundBuffer dr;                                               // звук капли
     dr.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/ZvonCoin.wav");
     Sound drop(dr);
-    drop.setVolume(60);
-     
-    SoundBuffer ht;                                               //çâóê óäàðà
+    drop.setVolume(60.f);
+
+    SoundBuffer ht;                                               //звук удара
     ht.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/Hit.wav");
     Sound hit(ht);
-    hit.setVolume(100);
+    hit.setVolume(100.f);
 
-    Texture f;                                                    // áåêãðàóíä
+    Texture f;                                                    // бекграунд
     f.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/fon2.png");
     Sprite fon(f);
 
-    Earth l;                                                      // êëàññ "ïî÷âû"
+    Earth l;                                                      // класс "почвы"
     l.loadFile();
     l.randEarthObj();
 
-    Texture m;                                                    // Ïåðñîíàæ
+    Texture m;                                                    // Персонаж
     m.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/man.png");
-    Sprite man(m);                                                // èñõîäíèê òåêñòóð äëÿ ïåðñîíàæà
+    Sprite man(m);                                                // исходник текстур для персонажа
 
-    Boost boost;                                                  // êëàññ áóñòîâ
+    Boost boost;                                                  // класс бустов
     boost.loadFile();
     boost.randBoostObj();
 
-    float y = 540, frame = 0, py = 0, dy = 0, spead = 1, fx = 0;  // ó - èçíà÷àëüíàÿ êîîðäèíàòà Ïåðñîíàæà, frame - ñêîðîñòü ñìåíû è ñìåíà êàäðîâ, py - íûíåøíåå
-    bool up = false, down = false, anim = true, scoreX10 = false; // ïîëîæåíèå Ïåðñ íà îñè Ó, dy - ê êîòîðîìó ñòðåìèòñÿ, fx - ÷àñòîòà êàäðîâ íàäïèñåé,
-    int game = 0;                                                 // up, down, anim - àíèìàöèÿ Ïåðñîíàæà, scoreX10 - óâåëè÷åíèå áóñòà íà 10
-                                                                  // game - ïåðåìåííàÿ "ñëåäÿùàÿ" çà èãðîâûìè ñîáûòèÿìè, îòâå÷àåò çà íà÷ëî è êîíåö èãðû
-    Texture t;                                                    // Òåñò "Óðîâåíü 1"
+    float y = 540, frame = 0, py = 0, dy = 0, spead = 1, fx = 0;  // у - изначальная координата Персонажа, frame - скорость смены и смена кадров, py - нынешнее
+    bool up = false, down = false, anim = true, scoreX10 = false; // положение Перс на оси У, dy - к которому стремится, fx - частота кадров надписей,
+    int game = 0;                                                 // up, down, anim - анимация Персонажа, scoreX10 - увеличение буста на 10
+                                                                  // game - переменная "следящая" за игровыми событиями, отвечает за начло и конец игры
+    Texture t;                                                    // Тест "Уровень 1"
     t.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/textLev1.png");
     Sprite text(t);
-    text.setPosition(370, 400);                                   // ðàñïîëîæåíèå íàäïèñè íà ýêðàíå
+    text.setPosition(370.f, 400.f);                                   // расположение надписи на экране
 
-    Blocks bk;                                                    // êëàññ ïðåïÿòñòâèÿé â âèäå áëîêîâ
+    Blocks bk;                                                    // класс препятствияй в виде блоков
     bk.loadFile();
     bk.randBlockObj();
 
-    Texture go;                                                   // òåêñò Ãåéì Îâåð
+    Texture go;                                                   // текст Гейм Овер
     go.loadFromFile("C:/Users/Lenovo/Desktop/MyPR/GameRunner/Paint/GO.png");
     Sprite gameover(go);
-    gameover.setPosition(290, 450);                               // ðàñïîëîæåíèå íàäïèñè íà ýêðàíå
+    gameover.setPosition(290.f, 450.f);                               // расположение надписи на экране
 
-    Score sc;                                                     // êëàññ "Ñ÷¸ò÷èê î÷êîâ"
+    Score sc;                                                     // класс "Счётчик очков"
     sc.loadFile();
     sc.ScoreObjStart();
 
-    while (window.isOpen())                                       // ïîêà îòêðûòî îêíî
+    while (window.isOpen())                                       // пока открыто окно
     {
         Event event{};
         while (window.pollEvent(event))
         {
             if (event.type == Event::Closed)
                 window.close();
-            UpDownMovement(game, event, jump, pl, down, up, dy, py);// ô-ÿ àíèìàöèè ñòàðòîâîãî îêíà è äâèæåíèÿ ââåðõ è âíèç
+            UpDownMovement(game, event, jump, pl, down, up, dy, py);// ф-я анимации стартового окна и движения вверх и вниз
         }
 
         ManAnimationRun(man, l, bk, hit, frame, py, dy, game, drop, spead, y, anim, up, down, GameLocation, boost, scoreX10, sc);
-                                                                  // ô-ÿ àíèìàöèè âñåõ îáúåêòîâ
-        if (GameLocation == 2)                                    // Åñëè GameLocation = 2 òî çàêðûâàåì ýòî îêíî è ïåðåõîäèì íà ñëåäóþùèé óðîâåíü
+        // ф-я анимации всех объектов
+        if (GameLocation == 2)                                    // Если GameLocation = 2 то закрываем это окно и переходим на следующий уровень
             break;
 
-        window.draw(fon);                                         // èçîáðàæåíèå áåêãðàóíäà
+        window.draw(fon);                                         // изображение бекграунда
 
-        l.draw(window);                                           // èçîáðàæåíèå ïî÷âû
+        l.draw(window);                                           // изображение почвы
 
-        man.setPosition(200.f, y);                                // ïîñòîÿííàÿ êîîðäèíàòà Ïåðñîíàæà ïî îñè Õ
-        window.draw(man);                                         // èçîáðàæåíèå Ïåðñîíàæà
+        man.setPosition(200.f, y);                                // постоянная координата Персонажа по оси Х
+        window.draw(man);                                         // изображение Персонажа
         if (game == 0)
         {
             fx += 0.005f;
             if (fx > 5)
                 fx -= 5;
-            if (fx < 3) window.draw(text);                        // àíèìàöèÿ òåêñòà "Ëåâ 1 íàæìèòå äëÿ ïðîäîëæåíèÿ"
+            if (fx < 3) window.draw(text);                        // анимация текста "Лев 1 нажмите для продолжения"
         }
 
-        boost.draw(window);                                       // èçîáðàæåíèå áóñòîâ
-        bk.draw(window);                                          // èçîáðàæåíèå ïðåïÿòñòâèé
-        sc.draw(window);                                          // èçîáðàæåíèå ñ÷¸òà
+        boost.draw(window);                                       // изображение бустов
+        bk.draw(window);                                          // изображение препятствий
+        sc.draw(window);                                          // изображение счёта
 
-        if (game == 2)             
+        if (game == 2)
         {
-            window.draw(gameover);                                // åñëè game = 2, âûâîäèì íàäïèñü Ãåéì Îâåð
-            if (event.type == Event::MouseButtonPressed)          // îæèäàåì íàæàòèÿ êëàâèíè ìûøè
+            window.draw(gameover);                                // если game = 2, выводим надпись Гейм Овер
+            if (event.type == Event::MouseButtonPressed)          // ожидаем нажатия клавини мыши
                 if (Keyboard::Key::Left)
                 {
-                    GameLocation = 1;                             // ïîñëå ÷åãî âîçâðàùàåì  GameLocation = 1
-                    window.clear();                               // î÷èùàåì è çàêðûâàåì îêíî
+                    GameLocation = 1;                             // после чего возвращаем  GameLocation = 1
+                    window.clear();                               // очищаем и закрываем окно
                     window.close();
                 }
         }
